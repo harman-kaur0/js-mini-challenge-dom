eaa4480fc1b13c01760f0da24e9419a4a3bee300
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-10 23:45:43 -0700

    Completed

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("#header");
+  console.log("Here's your header:", header);
+  /***** Deliverable 2 *****/
+  header.style.color = "red";
+  /***** Deliverable 3 *****/
+  console.log("the player object looks like this:", player);
+  const h2 = document.querySelector("h2");
+  const em = document.querySelector("em");
+  const h4 = document.querySelector("h4");
+  const img = document.querySelector("img");
+  const ul = document.querySelector("#goals");
 
+  h2.innerText = player["name"];
+  em.innerText = player["nickname"];
+  img.src = player["photo"];
+  img.alt = player["name"];
+  player["goals"].forEach(function (data) {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const a = document.createElement("a");
+    li.setAttribute("data-id", data["id"]);
+    p.innerText = data["description"];
+    a.href = data["link"];
+    a.innerText = data["link"];
+    a.target = "_blank";
 
-/***** Deliverable 2 *****/
-
-
-/***** Deliverable 3 *****/
-// console.log("the player object looks like this:", player)
-
+    ul.appendChild(li);
+    li.appendChild(p);
+    li.appendChild(a);
+  });
+  const ids = document.querySelector("[data-id = '3']");
+  ids.remove();
+});
 
 /***** Deliverable 4 *****/
-
 
 /***** Deliverable 5 *****/
